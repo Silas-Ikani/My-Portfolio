a19ec94365f5ee4cc3eb156ad03cf82d43a546e1
Silas Ikani <silasikani44@gmail.com>
2025-04-24 12:36:44 +0100

    Add new changes

--- JS/script.js
+++ JS/script.js
@@ -18,35 +18,35 @@ function toggleMenu() {
 
 
 // Typing Effect
-const words = ["a Web Developer", "Tech Enthusiast", "a Problem Solver"];
-let wordIndex = 0;
-let charIndex = 0;
-let typingText = document.querySelector(".typing-text");
+// const words = ["a Web Developer", "Tech Enthusiast", "a Problem Solver"];
+// let wordIndex = 0;
+// let charIndex = 0;
+// let typingText = document.querySelector(".typing-text");
 
-const typeEffect = () => {
-    if (charIndex < words[wordIndex].length) {
-        typingText.innerHTML = words[wordIndex].substring(0, charIndex + 1);
-        charIndex++;
-        setTimeout(typeEffect, 100);
-    } else {
-        setTimeout(eraseEffect, 2000);
-    }
-};
+// const typeEffect = () => {
+//     if (charIndex < words[wordIndex].length) {
+//         typingText.innerHTML = words[wordIndex].substring(0, charIndex + 1);
+//         charIndex++;
+//         setTimeout(typeEffect, 100);
+//     } else {
+//         setTimeout(eraseEffect, 2000);
+//     }
+// };
 
-const eraseEffect = () => {
-    if (charIndex > 0) {
-        typingText.innerHTML = words[wordIndex].substring(0, charIndex - 1);
-        charIndex--;
-        setTimeout(eraseEffect, 50);
-    } else {
-        wordIndex = (wordIndex + 1) % words.length;
-        setTimeout(typeEffect, 500);
-    }
-};
+// const eraseEffect = () => {
+//     if (charIndex > 0) {
+//         typingText.innerHTML = words[wordIndex].substring(0, charIndex - 1);
+//         charIndex--;
+//         setTimeout(eraseEffect, 50);
+//     } else {
+//         wordIndex = (wordIndex + 1) % words.length;
+//         setTimeout(typeEffect, 500);
+//     }
+// };
 
-document.addEventListener("DOMContentLoaded", () => {
-    setTimeout(typeEffect, 1000);
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//     setTimeout(typeEffect, 1000);
+// });
 
 // reveal effect
 function revealOnScroll() {
